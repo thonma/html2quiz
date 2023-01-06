d5d1576da9f79a25d749cc229a181ae42e628268
thonma <takumi.honma.w@gmail.com>
2023-01-06 16:54:27 +0900

    document.bodyをすべて上書きする方法だと元々の動作に影響を与えてしまうことがあるため対象要素のみを再描画するように変更

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # html2quiz
+
+## 概要
 - 【HTML】を【クイズ】形式に変換する【Chrome】拡張
+
+# 注意事項
+
+## 一部サイトでは動作しません
+- 動作しないサイト: github.com, 
+- 下記の条件のいずれかに該当するサイトでのみ動作する
+  - Conetnt-Security-Policy が設定されていない
+  - Conetnt-Security-Policy が設定されている かつ `script-src 'unsafe-inline'` が含まれている
--- h2q_background.js
+++ h2q_background.js
@@ -3,18 +3,36 @@
  */
 chrome.action.onClicked.addListener((tab) => {
   /**
-   * ページ内の【〇〇】をクイズに変換する
-   * 
-   * NOTE: document.body.innerHTML のサイズが大きいと重くなりそう
+   * ページ内の【〇〇】をクイズ形式に変換して描画する
    */
   const toQuiz = () => {
+    // =========================================================
     // <style>をページ内に追加
+    // =========================================================
     const styleHtmlStr = `<style> .chrome_ext_h2q_btn * { visibility: hidden; } </style>`;
     document.head.insertAdjacentHTML(`beforeend`, styleHtmlStr);
 
-    // <body>内部のHTMLを文字列として取得
-    const bodyHtmlStr = document.body.innerHTML;
+    // =========================================================
+    // テキストに 【○○】 を含む要素を取得
+    // ※<script>, <link> などは除外する
+    // =========================================================
+    const elems = document.evaluate(`.//*[text()[contains(., '【')] and text()[contains(., '】')]]`, document.body, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    const elemArr = [];
+    let e = null;
+    while (e = elems.iterateNext()) {
+      // TODO: 他にも除外するべきタグがあれば追記する
+      const excludedTags = [
+        'LINK', 'SCRIPT', 'NOSCRIPT', 'IFRAME'
+      ];
+      if (excludedTags.includes(e.tagName)) {
+        continue;
+      }
+      elemArr.push(e);
+    }
 
+    // =========================================================
+    // クイズ形式にして再描画
+    // =========================================================
     // ボタンのstyle属性値
     const btnStyleAttr = [
       `color: transparent`,
@@ -24,20 +42,22 @@ chrome.action.onClicked.addListener((tab) => {
       `font-weight: bold`,
       `border: none`,
       `min-width: 4rem`,
+      `cursor: pointer`,
       `user-select: none` // 選択できないようにする (選択すると答えが分かってしまうため)
     ].join(`; `);
 
     // ボタンのonclick属性値
     const onclickCode = `this.style.color = null; this.style.userSelect = null; this.classList.remove('chrome_ext_h2q_btn')`;
+    elemArr.forEach(e => {
+      // 【〇〇】を<span>に変換
+      const newHtmlStr = e.innerHTML
+        .replace(/【/g, `<span class="chrome_ext_h2q_btn" style="${btnStyleAttr}" onclick="${onclickCode}">`)
+        .replace(/】/g, `</span>`);
 
-    // 【〇〇】を<button>に変換
-    const newBodyHtmlStr = bodyHtmlStr
-      .replace(/【/g, `<button class="chrome_ext_h2q_btn" style="${btnStyleAttr}" onclick="${onclickCode}">`)
-      .replace(/】/g, `</button>`);
-
-    // 描画
-    document.body.innerHTML = ``;
-    document.body.insertAdjacentHTML(`beforeend`, newBodyHtmlStr);
+      // 描画
+      e.innerHTML = ``;
+      e.insertAdjacentHTML(`beforeend`, newHtmlStr);
+    });
   };
 
   // アクティブなタブ上で toQuiz() を実行する
